ea81ca13efc2c2b40bfcdbba4717f5e90581f476
Merrick <k.zhukov@bssys.com>
2019-10-03 17:13:54 +0300

    fix example app

--- example/src/App.js
+++ example/src/App.js
@@ -1,20 +1,11 @@
 import React, { Component } from 'react'
-import { Provider, useDialog, statuses }  from 'react-modaly';
 
-const Button = () => {
-  const { open } = useDialog();
-
-  open().then(({ action, values }) => {
-
-  })
-
-}
 
 export default class App extends Component {
   render () {
     return (
       <div>
-        <ExampleComponent text='Modern React component module' />
+        <h1>Example app</h1>
       </div>
     )
   }

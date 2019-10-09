3afde4347a8363216986fe8c23b9eedbac09d3d5
Konstantine <K.Zhukov@bssys.com>
2019-10-09 13:33:08 +0300

    fix: fix readme for npm

--- example/src/modals/Layout.js
+++ example/src/modals/Layout.js
@@ -1,19 +1,13 @@
 import React from 'react';
 
-import { Dialog } from "@blueprintjs/core";
+import { Dialog } from '@blueprintjs/core';
 
 export default ({ close, component: Component, success, cancel, instanceName }) => {
   return (
-    <Dialog
-      icon="info-sign"
-      onClose={close}
-      title=""
-      className={instanceName}
-      isOpen
-    >
+    <Dialog icon="info-sign" onClose={close} title="" className={instanceName} isOpen>
       <React.Suspense fallback={'loading'}>
         <Component success={success} cancel={cancel} />
       </React.Suspense>
     </Dialog>
-  )
+  );
 };
--- example/src/modals/config.js
+++ example/src/modals/config.js
@@ -3,5 +3,5 @@ import React from 'react';
 export const exampleInstanceName = 'modal/example';
 
 export default {
-  [exampleInstanceName]: React.lazy(() => import('./Example'))
+  [exampleInstanceName]: React.lazy(() => import('./Example')),
 };
